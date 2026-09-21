593584ebfc6973390b5bb40d9bb88a465224d48b
Tefin bose <tefinbose2003@gmail.com>
2026-09-21 17:45:58 +0530

    Update CORS middleware to specify allowed origins and enable credentials

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,15 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // --------------------
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "mul-berries-client.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // --------------------
